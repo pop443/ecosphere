d264c8beec600bc7410c0351345e3294bd43139d
pop443 <xzparty@sina.com>
2017-02-28 10:04:17 +0800

    thrift2 协议bug

--- hbase/src/main/java/com/xz/hbase/thrift/Test.java
+++ hbase/src/main/java/com/xz/hbase/thrift/Test.java
@@ -6,7 +6,10 @@ import org.apache.hadoop.hbase.thrift2.generated.THBaseService;
 import org.apache.hadoop.hbase.thrift2.generated.TResult;
 import org.apache.thrift.TException;
 import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TCompactProtocol;
 import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TFramedTransport;
+import org.apache.thrift.transport.THttpClient;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
 
@@ -19,12 +22,20 @@ public class Test {
     public static void main(String[] args) {
         try {
             TTransport socket = new TSocket("172.32.148.163", 9090);
-            TProtocol protocol = new TBinaryProtocol(socket, true, true);// 注意这里
+            // THRIFT-601 http 协议访问 thrift 接口 内存溢出bug  启用 framed/compact protocol 协议
+            boolean framed = true ;
+            if (framed){
+                socket = new TFramedTransport(socket);
+            }
+            TProtocol protocol = new TBinaryProtocol(socket,true,true);// 注意这里
+            if (framed){
+                protocol = new TCompactProtocol(socket);
+            }
             THBaseService.Iface client = new THBaseService.Client(protocol);
             socket.open();
-            ByteBuffer table = ByteBuffer.wrap("kylin_metadata".getBytes());
+            ByteBuffer table = ByteBuffer.wrap("hbase".getBytes());
             TGet get = new TGet();
-            get.setRow("/table/DEFAULT.KYLIN_SALES.json".getBytes());
+            get.setRow("11000-20030505121207".getBytes());
             TResult result = client.get(table, get);
             System.out.println("row = " + new String(result.getRow()));
             for (TColumnValue resultColumnValue : result.getColumnValues()) {
