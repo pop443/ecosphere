9b1b5b943cf4029c0771bc7cd01779824674c1ab
pop443 <xzparty@sina.com>
2020-06-10 19:27:56 +0800

    1

--- kafka/src/main/java/com/xz/kafka/init/Client.java
+++ kafka/src/main/java/com/xz/kafka/init/Client.java
@@ -1,68 +1,75 @@
 package com.xz.kafka.init;
 
 import com.xz.kafka.conf.KafkaConf;
-import kafka.admin.TopicCommand;
-import kafka.admin.TopicCommand.TopicCommandOptions;
-import kafka.utils.ZkUtils;
-
-import kafka.zk.KafkaZkClient;
-import kafka.zookeeper.ZooKeeperClient;
-import org.I0Itec.zkclient.ZkClient;
-import org.I0Itec.zkclient.ZkConnection;
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.admin.*;
+import org.apache.kafka.common.KafkaFuture;
 import org.apache.kafka.common.utils.SystemTime;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.function.BiConsumer;
+
 public class Client {
 
 
-	private KafkaZkClient kafkaZkClient ;
+	private AdminClient adminClient;
+	private String topicName="test";
 
 	@Before
 	public void before(){
-		ZooKeeperClient zooKeeperClient = new ZooKeeperClient(KafkaConf.getZookeeper(),30000,30000,30000,new SystemTime(),"","") ;
-		kafkaZkClient = new KafkaZkClient(zooKeeperClient,false,new SystemTime());
-	}
-	@Test
-	public void listTopic(){
-		String [] options = new String[]{
-				"--zookeeper",
-				KafkaConf.getZookeeper()
-		};
-		TopicCommandOptions topicCommandOptions = new TopicCommandOptions(options) ;
-		TopicCommand.listTopics(kafkaZkClient, topicCommandOptions);
+		Properties properties = new Properties();
+		properties.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, KafkaConf.getBootstrapServers());
+		adminClient = AdminClient.create(properties);
 	}
 	@Test
 	public void createTopic(){
-		String[] options = new String[]{
-				"--zookeeper",
-				KafkaConf.getZookeeper(),
-				"--topic",
-				KafkaConf.getTopic(),
-				"--partitions",
-				"2",
-				"--replication-factor",
-				"1"
-		};
-		TopicCommandOptions topicCommandOptions = new TopicCommandOptions(options) ;
-		TopicCommand.createTopic(kafkaZkClient, topicCommandOptions);
+		NewTopic newTopic = new NewTopic(topicName,4, (short) 1);
+		List<NewTopic> newTopicList = new ArrayList<>();
+		newTopicList.add(newTopic);
+		CreateTopicsResult createTopicsResult = adminClient.createTopics(newTopicList);
+		System.out.println(createTopicsResult.all());
+	}
+	@Test
+	public void listTopic(){
+		ListTopicsOptions listTopicsOptions = new ListTopicsOptions();
+		listTopicsOptions.listInternal(true);
+		ListTopicsResult result = adminClient.listTopics(listTopicsOptions);
+		try {
+			Collection<TopicListing> list = result.listings().get();
+			list.forEach(topicListing -> {
+				System.out.println(topicListing.name());
+			});
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Test
 	public void deleteTopic(){
-		String[] options = new String[]{
-				"--zookeeper",
-				KafkaConf.getZookeeper(),
-				"--topic",
-				KafkaConf.getTopic()
-		};
-		TopicCommandOptions topicCommandOptions = new TopicCommandOptions(options) ;
-		TopicCommand.deleteTopic(kafkaZkClient, topicCommandOptions);
+		List<String> deleteList = new ArrayList<>();
+		deleteList.add(topicName) ;
+		DeleteTopicsResult deleteTopicsResult = adminClient.deleteTopics(deleteList);
+		System.out.println(deleteTopicsResult.all());
+		/*try {
+			for(Map.Entry<String,KafkaFuture<Void>> e : deleteTopicsResult.values().entrySet()){
+                String topic=e.getKey();
+                KafkaFuture<Void> future=e.getValue();
+                Void v = future.get();
+				System.out.println(topic+"---"+v.toString());
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}*/
 	}
 	
 	@After
 	public void after(){
-		kafkaZkClient.close();
+		adminClient.close();
 	}
 }
--- kafka/src/main/java/com/xz/kafka/partitioner/MyPartitioner.java
+++ kafka/src/main/java/com/xz/kafka/partitioner/MyPartitioner.java
@@ -1,12 +1,11 @@
 package com.xz.kafka.partitioner;
 
-import kafka.producer.Partitioner;
 
 /**
  * falcon -- 2016/12/29.
  */
-public class MyPartitioner implements Partitioner {
-    @Override
+public class MyPartitioner  {
+
     public int partition(Object key, int num) {
         return Math.abs(key.hashCode()) % num ;
     }
